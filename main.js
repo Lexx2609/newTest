4820d3c1fdb3ee9e3797cf7ddc74122cc2be8499
Lexx2609 <2433053@mail.ru>
2023-06-20 18:01:54 +0500

    if else

--- main.js
+++ main.js
@@ -2,14 +2,20 @@
 // console.log('Добро ' + 'пожаловать ' + 'на курс ' + 'Алексей')
 // let data = new Date()
 // alert(data)
-let element = document.getElementById('one')
-element.id = 'Hello World'
-console.log(typeof undefined)
-console.log(typeof 'строка')
-console.log(typeof 95)
-console.log(typeof 95n)
-console.log(typeof false)
-console.log(typeof null)
-console.log(typeof Symbol())
-console.log(typeof Math)
-console.log(typeof alert)
+// let element = document.getElementById('one')
+// element.id = 'Hello World'
+// console.log(typeof undefined)
+// console.log(typeof 'строка')
+// console.log(typeof 95)
+// console.log(typeof 95n)
+// console.log(typeof false)
+// console.log(typeof null)
+// console.log(typeof Symbol())
+// console.log(typeof Math)
+// console.log(typeof alert)
+let a = prompt('Введите число')
+if (a >= 100) {
+    alert(a)
+} else {
+    console.log(a)
+}
