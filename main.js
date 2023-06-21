3234d4e57992f35b2ce8b65f954ec588dceee8bb
Lexx2609 <2433053@mail.ru>
2023-06-21 18:30:40 +0500

    true false

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-// console.log('09')
-// console.log('Добро ' + 'пожаловать ' + 'на курс ' + 'Алексей')
-// let data = new Date()
-// alert(data)
-// let element = document.getElementById('one')
-// element.id = 'Hello World'
-// console.log(typeof undefined)
-// console.log(typeof 'строка')
-// console.log(typeof 95)
-// console.log(typeof 95n)
-// console.log(typeof false)
-// console.log(typeof null)
-// console.log(typeof Symbol())
-// console.log(typeof Math)
-// console.log(typeof alert)
-let a = prompt('Введите число')
-if (a >= 100) {
-    alert(a)
-} else {
-    console.log(a)
-}
+let a = prompt('Введите a')
+let b = prompt('Введите b')
+a+b <= 15 ? console.log('Да'): console.log('Нет')
+a === b ? console.log(true): console.log(false)
+
