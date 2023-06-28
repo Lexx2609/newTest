07a794675610d260becd33f3e53ea5efa4e1cabe
Lexx2609 <2433053@mail.ru>
2023-06-28 15:42:55 +0500

    switch

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
-let a = prompt('Введите a')
-let b = prompt('Введите b')
-a+b <= 15 ? console.log('Да'): console.log('Нет')
-a === b ? console.log(true): console.log(false)
-
+// let a = '1'
+// let b = prompt('Введите переменную')
+// switch(a) {
+//     case b:
+//         console.log('Верно')
+//         break
+//
+//     case b:
+//         console.log('Не верно')
+//         break
+//
+//     default:
+//         console.log('Не сработало')
+//         break
+// }
+// let a = +prompt('Введите месяц')
+// switch(a) {
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log('Зима')
+//         break
+//     case 4:
+//     case 5:
+//     case 6:
+//         console.log('Весна')
+//         break
+//     case 7:
+//     case 8:
+//     case 9:
+//         console.log('Лето')
+//         break
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log('Осень')
+//         break
+//
+//     default:
+//         console.log('Не сработало')
+//         break
+// }
+// let a = null
+// let b = undefined
+// let c = 1
+// alert(a ?? b ?? c)
